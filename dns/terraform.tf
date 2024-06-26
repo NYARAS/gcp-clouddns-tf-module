4e72cfecb1c57e32370bea7e0a608595baef2bf2
Calvine Otieno <nyarangaotieno@gmail.com>
2024-06-26 23:05:36 +0300

    feat: setup the module
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ dns/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.15.0"
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.10.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.25.2"
+    }
+  }
+}
